9874c3ef3d4f8dbdb74ac936d756e7e3ddeeae88
abedrivaz <abedrivaz123456@gmail.com>
2023-07-09 15:35:29 +0330

    implement react-router-dom and HomeScreen ProductScreen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Rating from './Rating'
 
+
 export default function Product(props) {
     const {product}=props
   return (
-    <div key={product._id} className="card">
-    <a href={`/product/${product._id}`}>
+   
+    <div key={product.slug} className="card">
+    <a href={`/product/${product.slug}`}>
       <img className="medium" src={product.image} alt="product" />
     </a>
     <div className="card-body">
-    <a href={`/product/${product._id}`}>
+    <a href={`/product/${product.slug}`}>
         <h2>{product.name}</h2>
       </a>
      <Rating rating={product.rating}  numReviews={ product.numReviews}></Rating>
       <div className="price">${product.price}</div>
     </div>
   </div>
+ 
   )
 }
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,8 +1,9 @@
 const data={
     products:[
         {
-            _id:'1',
+           // _id:'1',
             name: 'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
             category: 'Shirts',
             image: '/images/p1.jpg',
             price: 120,
@@ -13,8 +14,9 @@ const data={
             description: 'high quality product',
           },
           {
-            _id:'2',
+            //_id:'2',
             name: 'Adidas Fit Shirt',
+            slug:'adidas-fit-shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
             price: 100,
@@ -25,8 +27,9 @@ const data={
             description: 'high quality product',
           },
           {
-            _id:'3',
+           // _id:'3',
             name: 'Lacoste Free Shirt',
+            slug:'lacoste-free-shirt',
             category: 'Shirts',
             image: '/images/p3.jpg',
             price: 220,
@@ -37,8 +40,9 @@ const data={
             description: 'high quality product',
           },
           {
-            _id:'4',
+           // _id:'4',
             name: 'Nike Slim Pant',
+            slug:'nike-slim-pant',
             category: 'Pants',
             image: '/images/p4.jpg',
             price: 78,
@@ -48,30 +52,7 @@ const data={
             numReviews: 14,
             description: 'high quality product',
           },
-          {
-            id:'5',
-            name: 'Puma Slim Pant',
-            category: 'Pants',
-            image: '/images/p2.jpg',
-            price: 65,
-            countInStock: 5,
-            brand: 'Puma',
-            rating: 3.5,
-            numReviews: 10,
-            description: 'high quality product',
-          },
-          {
-            id:'6',
-            name: 'Adidas Fit Pant',
-            category: 'Pants',
-            image: '/images/p3.jpg',
-            price: 139,
-            countInStock: 12,
-            brand: 'Adidas',
-            rating: 5,
-            numReviews: 15,
-            description: 'high quality product',
-          },
+         
          
     ],
 };
